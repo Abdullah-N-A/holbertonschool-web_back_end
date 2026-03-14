58565186cc5620e46b5fd696906eb4d35a7ada27
Abdullah-N-A <Abdullahttlrx@gmail.com>
2026-03-14 22:11:33 +0300

    Update 9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,32 +1,49 @@
 export class HolbertonClass {
-  constructor(year, city) {
+  constructor(year, location) {
     this._year = year;
-    this._city = city;
+    this._location = location;
   }
 
   get year() {
     return this._year;
   }
 
-  get city() {
-    return this._city;
+  get location() {
+    return this._location;
   }
 }
 
-export class ClassList {
-  constructor(data) {
-    this._data = data.map(
-      (classItem) => new HolbertonClass(classItem.year, classItem.city),
-    );
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
   }
 
-  get data() {
-    return this._data;
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-export const listOfClasses = new ClassList([
-  { year: 1920, city: 'San Francisco' },
-  { year: 1965, city: 'Paris' },
-  { year: 2002, city: 'Lyon' },
-]);
+const listOfHolbertonClasses = [
+  new HolbertonClass(2020, 'San Francisco'),
+  new HolbertonClass(2019, 'San Francisco'),
+];
+
+export const listOfStudents = [
+  new StudentHolberton('Guillaume', 'Salva', listOfHolbertonClasses[0]),
+  new StudentHolberton('John', 'Doe', listOfHolbertonClasses[0]),
+  new StudentHolberton('Albert', 'Clinton', listOfHolbertonClasses[1]),
+  new StudentHolberton('Donald', 'Bush', listOfHolbertonClasses[1]),
+  new StudentHolberton('Jason', 'Sandler', listOfHolbertonClasses[1]),
+];
+
+export default listOfStudents;
