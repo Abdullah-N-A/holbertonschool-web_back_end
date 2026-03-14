d23d362a201e8e51578c6d339bb3f965189e5ae3
Abdullah-N-A <abdullahttlrx@gmail.com>
2026-03-14 21:55:05 +0300

    Fix task 6: correct template literal string and income value

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -1,10 +1,10 @@
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
-    income: '$119,816',
+    income: '$119,868',
     gdp: '$154.2 billion',
     capita: '$178,479',
   };
 
-  return `As of ${year}, it is estimated that ${budget.income} was the median household income for the San Francisco area. As a result, San Francisco was named the top tech employer in the 2017 report, with ${budget.gdp} in GDP and a per capita GDP of ${budget.capita}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
